12459d1c074b2e91752f0b37e61bac6774a14d35
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-07 00:44:32 -0300

    craindo sidebar da pagina

---
+++ src/@Types/allTypes.d.ts
@@ -0,0 +1,2 @@
+declare module '@heroicons/react'
+declare module '@heroicons/react/outline'
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { AppProps } from "next/app";
+import '../styles/globalStyles.scss';
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-    return (
-        <Component {...pageProps} />
-    )
+    return <Component {...pageProps} />
 }
 
 export default MyApp;
