8cde2996e1a757e56293806e8d084b5551c635fd
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-10 15:15:34 -0300

    adicionando botao logout e listagem de playlists na sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,11 +6,38 @@ import {
     LibraryIcon,
     PlusCircleIcon
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import useSpotify from '../hooks/useSpotify'
+
 
 const Sidebar = () => {
+    const spotifyApi = useSpotify()
+
+    const { data: session, status } = useSession()
+
+    const [ playlists, setPlaylists ] = useState([])
+
+    const [ playlistId, setPLaylistId ] = useState(null)
+
+    useEffect(() => {
+        if(spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPlaylists(data.body.items)
+            })
+        }
+    }, [session, spotifyApi])
+
     return (
-        <div className='text-gray-500 p-5 text-sm border-r border-gray-900'>
+        <div className='text-gray-500 p-5 text-sm border-r border-gray-900
+        overflow-y-scroll scrollbar-hide h-screen' >
             <div className='space-y-4'>
+                <button
+                    className='flex items-center space-x-2 hover:text-white'
+                    onClick={() => signOut()}
+                >
+                    <p>Logout</p>
+                </button>
                 <button className='flex items-center space-x-2 hover:text-white'>
                     <HomeIcon className='h-5 w-5'/>
                     <p>Home</p>
@@ -39,8 +66,13 @@ const Sidebar = () => {
                 </button>
 
                 <hr className='border-t-[0.1px] border-gray-900' />
-                <p className='cursor-pointer hover:text-white'>nome da playlist</p>
 
+                {playlists.map((playlist) => (
+                    <p
+                        key={playlist.id}
+                        className='cursor-pointer hover:text-white'
+                    >{playlist.name}</p>
+                ))}
             </div>
         </div>
     )
