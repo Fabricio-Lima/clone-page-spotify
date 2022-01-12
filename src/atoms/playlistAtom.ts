aa6229af11a522a0949adf4f0144aca00fe3565d
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-12 01:11:43 -0300

    criando atoms da listagem de playlist e de musica

---
+++ src/atoms/playlistAtom.ts
@@ -0,0 +1,12 @@
+import { atom } from "recoil";
+
+
+export const playlistState = atom({
+    key: 'playlistState',
+    default: null
+})
+
+export const playlistIdState = atom({
+    key: 'playlistIdAtom',
+    default: '37i9dQZF1DZ06evO197W3C'
+})
