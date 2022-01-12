096a145832e39a20695e0ecb0118153909619684
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-12 01:10:14 -0300

    adicionando recoil no arquivo _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,15 @@ import React from "react"
 import { AppProps } from "next/app"
 import '../styles/globalStyles.scss'
 import { SessionProvider } from 'next-auth/react'
+import { RecoilRoot } from "recoil"
 
 const MyApp: React.FC<AppProps> = ({ Component,
     pageProps: {session, ...pageProps} }) => {
     return (
         <SessionProvider session={session}>
-            <Component {...pageProps} />
+            <RecoilRoot>
+                <Component {...pageProps} />
+            </RecoilRoot>
         </SessionProvider>
     )
 }
