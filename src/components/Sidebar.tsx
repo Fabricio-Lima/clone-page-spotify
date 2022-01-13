34dcd619af6d54a91e0ba6d78d0cf8097205e6a2
Fabricio Lima <fabriciolc61@gmail.com>
2022-01-13 14:32:36 -0300

    alterando as cores dos icones heart e rss

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,9 +3,11 @@ import {
     SearchIcon,
     LibraryIcon,
     PlusCircleIcon,
-    HeartIcon,
     RssIcon
 } from '@heroicons/react/outline'
+
+import { HeartIcon } from '@heroicons/react/solid'
+
 import { signOut, useSession } from 'next-auth/react'
 import { useEffect, useState } from 'react'
 import { useRecoilState } from 'recoil'
@@ -54,11 +56,11 @@ const Sidebar = () => {
                     <p>Create Playlist</p>
                 </button>
                 <button className='flex items-center space-x-2 hover:text-white'>
-                    <HeartIcon className='h-5 w-5'/>
+                    <HeartIcon className='h-5 w-5 text-blue-500'/>
                     <p>Liked Songs</p>
                 </button>
                 <button className='flex items-center space-x-2 hover:text-white'>
-                    <RssIcon className='h-5 w-5'/>
+                    <RssIcon className='h-5 w-5 text-green-500'/>
                     <p>Your Episodes</p>
                 </button>
 
